b19e1ed1f74b3049ec6c3db9031784bdb7109b78
arne.natskar <arne.natskar@bekk.no>
2026-09-08 15:19:54 +0000

    Tillat at siden vises i iframe

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [{
+      source: '/:path*',
+      headers: [
+        { key: 'Content-Security-Policy', value: 'frame-ancestors *' },
+        { key: 'X-Frame-Options', value: 'ALLOWALL' },
+      ],
+    }];
+  },
 };
 
 export default nextConfig;
